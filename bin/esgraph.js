b4da9cb2ab1c9e60a376b7d768b66411a5070616
tiagorr99 <tiago.err@outlook.com>
2020-03-06 11:07:25 +0000

    Added a function that works the same as the CLI command of esgraph.

--- bin/esgraph.js
+++ bin/esgraph.js
@@ -49,6 +49,47 @@ process.stdin.on('end', () => {
   }
 });
 
+function generateDotCode(data) {
+	let source = data;
+  // filter out hashbangs
+  if (source.startsWith('#!')) {
+    source = `//${source.substring(2)}`;
+  }
+
+  var results = []
+
+  try {
+    const fullAst = parse(source, { range: true });
+    const functions = findFunctions(fullAst);
+
+    results.push('digraph cfg {');
+    results.push('node [shape="box"]');
+    const options = { counter: 0, source };
+    functions.concat(fullAst).forEach((ast, i) => {
+      let cfg;
+      let label = '[[main]]';
+      if (ast.type.includes('Function')) {
+        cfg = esgraph(ast.body);
+        const name = (ast.id && ast.id.name) || '';
+        const params = ast.params.map(p => p.name);
+        label = `function ${name}(${params})`;
+      } else {
+        cfg = esgraph(ast);
+      }
+
+      results.push(`subgraph cluster_${i}{`);
+      results.push(`label = "${label}"`);
+      results.push(esgraph.dot(cfg, options));
+      results.push('}');
+    });
+    results.push('}');
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  return results.join('\n')
+}
+
 function findFunctions(ast) {
   const functions = [];
   function handleFunction(node, recurse) {
